e0c7f35be4aebeb27e26cd626d80116e78193d6f
ryan0319 <ryan@ryanrussell.ca>
2022-10-24 14:02:32 -0230

    Add weather base template and styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.less';
+import React from "react";
+import "./App.less";
+import icon from "./icons/rainy-3.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header></header>
+      <div className="container-grid">
+        <div className="weather-today">
+          <h3 className="weather-today-title">Today</h3>
+          <div className="weather-container">
+            <div>
+              <img alt="Clouds" className="weather-icon" src={icon} />
+            </div>
+            <div>
+              <strong className="weather-today-degree">19&deg;</strong>
+              <div>Clouds</div>
+            </div>
+          </div>
+        </div>
+        <div className="weather-weekday">
+          <h4 className="weather-weekday-title">Wed</h4>
+          <img alt="Clouds" className="weather-icon" src={icon} />
+          <strong className="weather-weekday-degree">18&deg;</strong>
+        </div>
+        <div className="weather-weekday">
+          <h4 className="weather-weekday-title">Thu</h4>
+          <img alt="Clouds" className="weather-icon" src={icon} />
+          <strong className="weather-weekday-degree">18&deg;</strong>
+        </div>
+        <div className="weather-weekday">
+          <h4 className="weather-weekday-title">Fri</h4>
+          <img alt="Clouds" className="weather-icon" src={icon} />
+          <strong className="weather-weekday-degree">18&deg;</strong>
+        </div>
+        <div className="weather-weekday">
+          <h4 className="weather-weekday-title">Sat</h4>
+          <img alt="Clouds" className="weather-icon" src={icon} />
+          <strong className="weather-weekday-degree">18&deg;</strong>
+        </div>
+      </div>
     </div>
   );
 }
