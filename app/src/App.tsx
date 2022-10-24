fa9a2c670f969ccfcc5fa1eb6ef1e47248bbf2b5
ryan0319 <ryan@ryanrussell.ca>
2022-10-24 14:25:11 -0230

    Create calendar components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,45 +1,17 @@
 import React from "react";
 import "./App.less";
-import icon from "./icons/rainy-3.svg";
+import { getWeather } from "./api/Fetch";
+import Weather from "./components/Weather";
 
-function App() {
+const get = async () => {
+  console.log(await getWeather());
+};
+
+const App = () => {
+  get();
   return (
     <div className="container">
-      <header></header>
-      <div className="container-grid">
-        <div className="weather-today">
-          <h3 className="weather-today-title">Today</h3>
-          <div className="weather-container">
-            <div>
-              <img alt="Clouds" className="weather-icon" src={icon} />
-            </div>
-            <div>
-              <strong className="weather-today-degree">19&deg;</strong>
-              <div>Clouds</div>
-            </div>
-          </div>
-        </div>
-        <div className="weather-weekday">
-          <h4 className="weather-weekday-title">Wed</h4>
-          <img alt="Clouds" className="weather-icon" src={icon} />
-          <strong className="weather-weekday-degree">18&deg;</strong>
-        </div>
-        <div className="weather-weekday">
-          <h4 className="weather-weekday-title">Thu</h4>
-          <img alt="Clouds" className="weather-icon" src={icon} />
-          <strong className="weather-weekday-degree">18&deg;</strong>
-        </div>
-        <div className="weather-weekday">
-          <h4 className="weather-weekday-title">Fri</h4>
-          <img alt="Clouds" className="weather-icon" src={icon} />
-          <strong className="weather-weekday-degree">18&deg;</strong>
-        </div>
-        <div className="weather-weekday">
-          <h4 className="weather-weekday-title">Sat</h4>
-          <img alt="Clouds" className="weather-icon" src={icon} />
-          <strong className="weather-weekday-degree">18&deg;</strong>
-        </div>
-      </div>
+      <Weather />
     </div>
   );
 }
