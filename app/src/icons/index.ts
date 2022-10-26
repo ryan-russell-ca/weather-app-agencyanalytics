8c3262af2b36b990ec5dcb9621eb176ff62919cb
ryan0319 <ryan@ryanrussell.ca>
2022-10-25 22:42:45 -0230

    Add weather icons

---
+++ app/src/icons/index.ts
@@ -0,0 +1,41 @@
+import _01d from "./01d.svg";
+import _01n from "./01n.svg";
+import _02d from "./02d.svg";
+import _02n from "./02n.svg";
+import _03d from "./03d.svg";
+import _03n from "./03n.svg";
+import _04d from "./04d.svg";
+import _04n from "./04n.svg";
+import _09d from "./09d.svg";
+import _09n from "./09n.svg";
+import _10d from "./10d.svg";
+import _10n from "./10n.svg";
+import _11d from "./11d.svg";
+import _11n from "./11n.svg";
+import _13d from "./13d.svg";
+import _13n from "./13n.svg";
+import _50d from "./50d.svg";
+import _50n from "./50n.svg";
+
+const icons = {
+  "01d": _01d,
+  "01n": _01n,
+  "02d": _02d,
+  "02n": _02n,
+  "03d": _03d,
+  "03n": _03n,
+  "04d": _04d,
+  "04n": _04n,
+  "09d": _09d,
+  "09n": _09n,
+  "10d": _10d,
+  "10n": _10n,
+  "11d": _11d,
+  "11n": _11n,
+  "13d": _13d,
+  "13n": _13n,
+  "50d": _50d,
+  "50n": _50n,
+};
+
+export default icons;
